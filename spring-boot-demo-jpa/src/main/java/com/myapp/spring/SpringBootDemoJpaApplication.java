29787513808abda40bd2bee98140d557813d7ae8
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2021-03-26 09:20:27 +0530

    spring boot project jpa repository

--- spring-boot-demo-jpa/src/main/java/com/myapp/spring/SpringBootDemoJpaApplication.java
+++ spring-boot-demo-jpa/src/main/java/com/myapp/spring/SpringBootDemoJpaApplication.java
@@ -1,13 +1,65 @@
 package com.myapp.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.myapp.spring.model.Product;
+import com.myapp.spring.model.Review;
+import com.myapp.spring.repository.ProductRepository;
 
 @SpringBootApplication
 public class SpringBootDemoJpaApplication {
+	
+	@Autowired
+	private ProductRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoJpaApplication.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			
+			Product oneplusNord =repository.findById(1).get();
+			
+			Review review1 = new Review("My Comment3");
+			Review review2 = new Review("My Comment4");
+			
+			Map<Integer, Review> reviews = new HashMap<>();
+			reviews.put(4, review1);
+			reviews.put(5, review2);
+			oneplusNord.setReviews(reviews);
+			
+			repository.save(oneplusNord);
+			
+		};
+		
+	}
 
 }
+
+//@SpringBootApplication = @Configuration, @ComponentScan, @EnableAutoConfiguration
+
+// Spring Boot Application StartUp Procedure
+
+// Step1. Entry Class which has main method where SpringApplication.run() will be executed
+
+// Step2. SpringBootApplication Annotation is processed and ComponentScan is loaded where it starts autoscanning of classes
+// like @Repository, @Service, @Controller, @RestController
+
+// Step3 Environment Properties are loaded (starts looking for application.properties)
+
+// Step4 Enable Auto Configuration will auto load the beans based on the environment setup
+
+// Step5 AutoWiring Of The Beans are completed
+
+// Step6 Starts The Embedded Tomcat Server
+
+// Step7 Application is ready for use
---
+++ spring-boot-demo-jpa/src/main/java/com/myapp/spring/model/Product.java
@@ -0,0 +1,189 @@
+package com.myapp.spring.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+// Persistent Class
+// Objects of Type Product will be persisted using hibernate
+@Entity
+@Table(name = "jpaproducts")
+public class Product {
+	
+	// Field Will Be mapped to a primary key
+
+	@Id
+	@Column(name = "PRODUCT_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int productId;
+	
+	@Column(name = "PRODUCT_NAME",nullable = false)
+	private String productName;
+	
+	@Column(name = "PRODUCT_PRICE",nullable = false)
+	private double price;
+	
+	@Column(name = "PRODUCT_DESCRIPTION",nullable = false)
+	private String description;
+	
+	
+	@Column(name = "STAR_RATING",nullable = false)
+	private double starRating;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(joinColumns =@JoinColumn(name="PRODUCT_ID"),
+	inverseJoinColumns =@JoinColumn(name = "REVIEW_ID") ,name = "product_reviews" )
+	private Map<Integer, Review> reviews = new HashMap<>();
+	
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Product( String productName, double price, String description, double starRating) {
+		
+		this.productName = productName;
+		this.price = price;
+		this.description = description;
+		this.starRating = starRating;
+	}
+
+
+	public int getProductId() {
+		return productId;
+	}
+
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+
+	public String getProductName() {
+		return productName;
+	}
+
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public double getStarRating() {
+		return starRating;
+	}
+
+
+	public void setStarRating(double starRating) {
+		this.starRating = starRating;
+	}
+	
+	
+
+
+	public Map<Integer, Review> getReviews() {
+		return reviews;
+	}
+
+
+	public void setReviews(Map<Integer, Review> reviews) {
+		this.reviews = reviews;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, price, productId, productName, starRating);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Product))
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(description, other.description)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& productId == other.productId && Objects.equals(productName, other.productName)
+				&& Double.doubleToLongBits(starRating) == Double.doubleToLongBits(other.starRating);
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [productId=");
+		builder.append(productId);
+		builder.append(", productName=");
+		builder.append(productName);
+		builder.append(", price=");
+		builder.append(price);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append(", starRating=");
+		builder.append(starRating);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	
+	
+
+//	@Override
+//	public String toString() {
+//		StringBuilder builder = new StringBuilder();
+//		builder.append("Product [productId=");
+//		builder.append(productId);
+//		builder.append(", productName=");
+//		builder.append(productName);
+//		builder.append(", price=");
+//		builder.append(price);
+//		builder.append(", description=");
+//		builder.append(description);
+//		builder.append(", starRating=");
+//		builder.append(starRating);
+//		builder.append("]");
+//		return builder.toString();
+//	}
+	
+	
+	
+	
+	
+	
+
+}
